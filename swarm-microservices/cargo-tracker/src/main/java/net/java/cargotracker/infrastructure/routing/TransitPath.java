bb64a6611e2d8036ffb23f501a0b6c2cced83c94
Reza Rahman <reza_rahman@lycos.com>
2016-10-31 15:58:19 -0400

    Cleanup.

--- swarm-microservices/cargo-tracker/src/main/java/net/java/cargotracker/infrastructure/routing/TransitPath.java
+++ swarm-microservices/cargo-tracker/src/main/java/net/java/cargotracker/infrastructure/routing/TransitPath.java
@@ -4,7 +4,6 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-// @XmlRootElement
 public class TransitPath implements Serializable {
 
     private List<TransitEdge> transitEdges;
