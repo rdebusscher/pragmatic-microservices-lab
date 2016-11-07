45adbc7e37fa41a075a09ad939a6a5745c954f67
Reza Rahman <reza@rahmannet.net>
2016-11-06 21:21:04 -0500

    Docker

--- complex-concepts/cargo-tracker/src/main/java/net/java/cargotracker/infrastructure/routing/ExternalRoutingService.java
+++ complex-concepts/cargo-tracker/src/main/java/net/java/cargotracker/infrastructure/routing/ExternalRoutingService.java
@@ -56,6 +56,7 @@ public class ExternalRoutingService implements RoutingService {
         JsonArray discoveryData = pathFinderDiscoveryResource
                 .request(MediaType.APPLICATION_JSON).get(JsonArray.class);
         String address = discoveryData.getJsonObject(0).getString("ServiceAddress");
+        // String address = "172.17.0.1";
         int port = discoveryData.getJsonObject(0).getInt("ServicePort");
 
         graphTraversalResource = jaxrsClient.target(
